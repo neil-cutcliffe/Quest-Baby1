e63ee55e8a17ece7f53f0cf61af4f9f40a1423c2
Neil Cutcliffe <neil.cutcliffe@deepspacesparkle.com>
2023-06-05 14:45:54 -0700

    Imported from Quest

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 import questTheme from 'src/MyDesignSystemLightTheme';
+import CardPost from 'src/components/CardPost/CardPost';
 
 function App() {
   return (
     <HelmetProvider>
       <StyledEngineProvider injectFirst>
         <ThemeProvider theme={questTheme}>
-          <Routes></Routes>
+          <Routes>
+            <Route path="/" element={<CardPost />} />
+          </Routes>
         </ThemeProvider>
       </StyledEngineProvider>
     </HelmetProvider>
